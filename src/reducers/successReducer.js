1cc116b577752b09e3c60d327331a01c2506104e
Charles Muzhanye <charles@Charless-iMac.local>
2019-07-23 17:24:01 +0200

    added store and reducer to app

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @param {array} state - Array of guessed words
+ * @param {object} action = action to be fired
+ * @returns {boolean} - new success state
+ */
+
+export default(state, action)=>{
+    return null
+}
